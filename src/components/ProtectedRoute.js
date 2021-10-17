29e5c5a4290ed25f73817006585c77d287f30b7a
Nikita Ershov <67161360+Nikshov@users.noreply.github.com>
2021-10-17 15:25:29 +0300

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,18 +1,3 @@
-// import React from "react";
-// import { Route, Redirect } from "react-router-dom";
-
-// const ProtectedRoute = ({loggedIn, component: Component, ...props }) => {
-//   return (
-//     <Route>
-//       {() =>
-//         loggedIn ? <Component {...props} /> : <Redirect to="/sign-in" />
-//       }
-//     </Route>
-//   );
-// };
-
-// export default ProtectedRoute;
-
 import { Route, Redirect } from "react-router-dom";
 
 export default function ProtectedRoute({ component: Component, ...props }) {
